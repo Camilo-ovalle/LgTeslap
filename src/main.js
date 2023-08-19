d6d8bfbc9c267eda90495fb32da006cef846d8c5
camilo ovalle <camilo.enrique.ovalle@gmail.com>
2023-08-19 00:05:22 -0500

    Setting the Backend to avoid the cosr eror in the future

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import express from "express";
+import cors from "cors";
 import usersRoutes from "./routes/users.routes.js";
 
 const app = express();
 const port = 3000;
 
 app.listen(port);
+app.use(cors());
+
+app.get("/test", (req, res) => {
+  res.send("Hola mundo");
+});
 
 app.use(usersRoutes);
