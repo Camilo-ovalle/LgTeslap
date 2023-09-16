fa94015097e7083af1be494d95d556a37cb5e542
camilo ovalle <camilo.enrique.ovalle@gmail.com>
2023-09-15 20:53:24 -0500

    I installed the dependencies to deplo the backend and modifed the env variables to use on production

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const DB_NAME = process.env.DB_NAME || "LgTeslap";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "CA1015OV";
 export const DB_USER = process.env.DB_USER || "Camilo";
 export const DB_PORT = process.env.DB_PORT || 3306;
-export const APP_PORT = 3000;
+export const APP_PORT = process.env.APP_PORT ?? 3000;
 
 //Secret key for the token generator
 export const SECRET_KEY = "SecretKey";
