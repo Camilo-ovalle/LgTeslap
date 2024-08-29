0955f3985e8c2dc507909b526b155335a9a3067a
camilo ovalle <camilo.enrique.ovalle@gmail.com>
2024-08-29 18:09:01 -0500

    Update the data of the connection

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 //There are the variables that are used in the project to connect to the database and the port where the server will be hosted
-export const DB_HOST = process.env.DB_HOST || "172.172.92.27";
-export const DB_NAME = process.env.DB_NAME || "LgTeslap";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "CA1015OV";
-export const DB_USER = process.env.DB_USER || "Camilo";
+export const DB_HOST = process.env.DB_HOST || '172.172.92.27';
+export const DB_NAME = process.env.DB_NAME || 'LgTeslap';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'Password123';
+export const DB_USER = process.env.DB_USER || 'Camilo';
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const APP_PORT = process.env.APP_PORT || 3000;
 //Secret key for the token generator
-export const SECRET_KEY = "SecretKey";
+export const SECRET_KEY = 'SecretKey';
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,30 +1,30 @@
-import { pool } from "../connection/conection.js";
-import bcrypt from "bcryptjs";
-import { tokenGen } from "../libs/jwt.js";
+import { pool } from '../connection/conection.js';
+import bcrypt from 'bcryptjs';
+import { tokenGen } from '../libs/jwt.js';
 
 export const loginController = async (req, res) => {
   const { email, password } = await req.body;
 
   //Here we are looking if there is any email that match with the one sent by the client
   try {
-    await pool.query("USE LgTeslap");
+    await pool.query('USE LgTeslap');
     const [logInData] = await pool.query(
-      `SELECT COUNT(*) AS count FROM users WHERE user_email = '${email}'`
+      `SELECT COUNT(*) AS count FROM users WHERE user_email = '${email}'`,
     );
     const count = logInData[0].count;
     //If there is any match, we will do a second query to find the user information and compare the password
     if (count > 0) {
       const [useFound] = await pool.query(
-        `SELECT * FROM users WHERE user_email = '${email}'`
+        `SELECT * FROM users WHERE user_email = '${email}'`,
       );
 
       const isMatch = await bcrypt.compare(password, useFound[0].user_password);
       if (!isMatch) {
-        res.status(401).send("Invalid password");
+        res.status(401).send('Invalid password');
       } else {
         // if the password match  we create a token with the user id and we save it into a cookie
         const token = await tokenGen({ id: useFound[0].user_id });
-        res.cookie("token", token);
+        res.cookie('token', token);
         res.json({
           id: useFound[0].user_id,
           name: useFound[0].user_name,
@@ -34,7 +34,7 @@ export const loginController = async (req, res) => {
         });
       }
     } else {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ message: 'User not found' });
     }
   } catch (error) {}
 };
@@ -45,50 +45,50 @@ export const registerController = async (req, res) => {
     const cryptPassword = await bcrypt.hash(password, 10);
     // here we use the pool to connect to the database and create a query to insert the new user
 
-    await pool.query("USE LgTeslap");
+    await pool.query('USE LgTeslap');
     const [result] = await pool.query(
-      `INSERT INTO users(user_name, user_email, user_password) VALUES('${userName}', '${email}', '${cryptPassword}')`
+      `INSERT INTO users(user_name, user_email, user_password) VALUES('${userName}', '${email}', '${cryptPassword}')`,
     );
     // here we get the id of the new user to do a new query and get the user data
     const userId = result.insertId;
 
     const [userSaved] = await pool.query(
-      `SELECT user_id, user_name, user_email FROM users WHERE user_id = ${userId};`
+      `SELECT user_id, user_name, user_email FROM users WHERE user_id = ${userId};`,
     );
 
     //Here we create the token using an external function for the new user and send it to the frontend and it is saved into a cookie
     const token = await tokenGen({ id: userSaved[0].user_id });
-    res.cookie("token", token);
+    res.cookie('token', token);
     res.json({
       id: userSaved[0].user_id,
       name: userSaved[0].user_name,
       email: userSaved[0].user_email,
     });
   } catch (error) {
-    res.status(500).send("there was an error");
+    res.status(500).send('there was an error');
   }
 };
 
 export const logOutController = (req, res) => {
-  res.cookie("token", "", {
+  res.cookie('token', '', {
     expires: new Date(0),
   });
   return res.sendStatus(200);
 };
 
 export const profileController = async (req, res) => {
-  await pool.query("USE LgTeslap");
+  await pool.query('USE LgTeslap');
 
   const [count] = await pool.query(
-    `SELECT COUNT(*) AS count FROM users WHERE user_id = '${req.user.id}'`
+    `SELECT COUNT(*) AS count FROM users WHERE user_id = '${req.user.id}'`,
   );
 
   const counting = count[0].count;
   if (counting <= 0) {
-    res.json({ message: "Profile not found" });
+    res.json({ message: 'Profile not found' });
   } else {
     const [profile] = await pool.query(
-      `SELECT * FROM users WHERE user_id = '${req.user.id}' `
+      `SELECT * FROM users WHERE user_id = '${req.user.id}' `,
     );
     res.json({
       id: profile[0].user_id,
